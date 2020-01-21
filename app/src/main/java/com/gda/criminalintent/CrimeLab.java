afebdc801588fe3c5f1b8cd4f3e250b76faa9aba
gegad <geghetsikd@gmail.com>
2020-01-21 23:36:44 +0400

    Show new crime when pressing add menu item.

--- app/src/main/java/com/gda/criminalintent/CrimeLab.java
+++ app/src/main/java/com/gda/criminalintent/CrimeLab.java
@@ -14,12 +14,6 @@ public class CrimeLab {
 
     private CrimeLab(Context context) {
         mCrimeList = new ArrayList<Crime>();
-        for (int i=0; i < 20; i++) {
-            Crime crime = new Crime();
-            crime.setTitle("Crime # " + i);
-            crime.setSolved(i % 2 == 0);
-            mCrimeList.add(crime);
-        }
     }
 
     public static CrimeLab get(Context context) {
@@ -40,5 +34,9 @@ public class CrimeLab {
             }
         }
         return null;
+    }
+
+    public void addCrime(Crime c) {
+        mCrimeList.add(c);
     }
 }
